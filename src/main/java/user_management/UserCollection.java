6c748ff7d9cbd269f5d2900daf5e74987f202cf3
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-19 14:25:10 -0400

    Password and email checker probably working

--- src/main/java/user_management/UserCollection.java
+++ src/main/java/user_management/UserCollection.java
@@ -1,5 +1,8 @@
 package user_management;
 
+import user_management.security.Password;
+import user_management.security.UserAuthenticationFailedException;
+
 import java.util.ArrayList;
 
 public class UserCollection extends ArrayList<User>{
@@ -23,10 +26,33 @@ public class UserCollection extends ArrayList<User>{
     }
 
     public User attemptLogin(String email, String password) {
+        try {
+            for (int i = 0; i < this.size(); i++) {
+                if (this.get(i).getEmail().equals(email) && this.get(i).getPassword().equals(Password.hashPassword(password))) {
+                    return this.get(i);
+                } else {
+                    throw new UserAuthenticationFailedException();
+                }
+            }
+        } catch (UserAuthenticationFailedException e) {
+            System.out.println("Incorrect email or password");
+        }
         return null;
     }
 
     public int createUser(String name, String email, String password) {
-        return 0;
+        this.add(new User(this.size(), name, email, password));
+        return this.size();
     }
+
+    private boolean passwordGood(String password){
+        String regex = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
+        return password.matches(regex);
+    }
+
+    private boolean emailGood(String email){
+        String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+        return email.matches(regex);
+    }
+
 }
--- src/main/java/user_management/security/Authenticator.java
+++ src/main/java/user_management/security/Authenticator.java
@@ -3,8 +3,10 @@ package user_management.security;
 import user_management.User;
 
 public class Authenticator {
+
     public static boolean authenticate(User user, String passwordToCheck) {
-        return false;
+        return user.getPassword().matches(passwordToCheck);
     }
+
 }
 
